04bf818e0133b2e4cef286a771effa471a1fd9a0
Kiran Chauhan <kiranchauhan7310@gmail.com>
2025-06-20 09:46:32 +0530

    Function/Operators

---
+++ script.js
@@ -0,0 +1,23 @@
+
+greet()
+//   let name = "Kiran"
+//         alert(name)
+//         name = "Shubham"
+//         console.log(name)
+
+// Body / Definition of a Function
+function greet() {
+    console.log("Good Morning")
+}
+
+// calling of a function
+
+function add(){
+    let a = 20;
+    let b = 50
+
+    console.log(a+b)
+}
+
+
+add()
